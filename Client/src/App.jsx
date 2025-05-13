28e56fd210e45cc7d6cfc6bee4759cc63a3e7bcc
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-05-13 20:58:52 +0530

    folder structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./Pages/Home"));
 function App() {
   return (
     <BrowserRouter>
-      <Router>
-        <Route path="/" element={<h1>Home</h1>}></Route>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
         <Route path="about" element={<h1>about</h1>}></Route>
-      </Router>
+      </Routes>
     </BrowserRouter>
   );
 }
